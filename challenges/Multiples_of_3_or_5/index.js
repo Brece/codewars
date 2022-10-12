c42705bb996b9e7c39be2ec8522acb7a7af37911
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-12 22:00:56 +0200

    adds solution to Multiples_of_3_or_5

--- challenges/Multiples_of_3_or_5/index.js
+++ challenges/Multiples_of_3_or_5/index.js
@@ -1,1 +1,22 @@
 "use strict";
+function solution(number) {
+    if (number === 0) {
+        return 0;
+    }
+    ;
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+const case1 = solution(10); // 3, 5, 6, 9 = 23
+const case2 = solution(15); // 3, 5, 6, 9, 10, 12 = 45
+const case3 = solution(-1);
+const case4 = solution(20);
+console.log("case1:", case1);
+console.log("case2:", case2);
+console.log("case3:", case3);
+console.log("case4:", case4);
