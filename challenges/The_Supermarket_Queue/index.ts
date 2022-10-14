642e1825c6dadcef309b37bddee33510b3c36497
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-14 21:58:27 +0200

    adds files for The_Supermarket_Queue

---
+++ challenges/The_Supermarket_Queue/index.ts
@@ -0,0 +1,18 @@
+function queueTime(customers, n): void {
+
+    return;
+}
+
+const queueTime1 = queueTime(); // 
+const queueTime2 = queueTime(); // 
+const queueTime3 = queueTime(); // 
+const queueTime4 = queueTime(); // 
+const queueTime5 = queueTime(); // 
+const queueTime6 = queueTime(); // 
+
+console.log(queueTime1);
+console.log(queueTime2);
+console.log(queueTime3);
+console.log(queueTime4);
+console.log(queueTime5);
+console.log(queueTime6);
