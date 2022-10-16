a11f15bb4410e2388116c46d1d77cfcf98a89c12
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-16 21:39:14 +0200

    updates file structure

---
+++ challenges/Kyu_6/The_Supermarket_Queue/index.ts
@@ -0,0 +1,32 @@
+// TODO:
+function queueTime(customers: number[], n: number): number {
+    if (n === 1) {
+        return customers.reduce((a, b) => a + b);
+    }
+
+    let sum = 0;
+    let currentCustomers = customers.splice(0, n).sort((a, b) => a - b);
+    let rest = customers.slice(n);
+    if (rest.length > 0) {
+        // substract smallest number from all customers
+        currentCustomers.map(n => n - currentCustomers[0] ? 0 : n - currentCustomers[0]);
+        // replace smallest number (all zeros) with next customer(s) and repeat until rest is empty
+        // store passed time in sum
+    }
+
+    return 0;
+}
+
+const queueTime1 = queueTime([5,3,4], 1); // 12
+const queueTime2 = queueTime([10,2,3,3], 2); // 10
+const queueTime3 = queueTime([2,3,10], 2); // 12
+// const queueTime4 = queueTime(); // 
+// const queueTime5 = queueTime(); // 
+// const queueTime6 = queueTime(); // 
+
+console.log(queueTime1);
+console.log(queueTime2);
+console.log(queueTime3);
+// console.log(queueTime4);
+// console.log(queueTime5);
+// console.log(queueTime6);
