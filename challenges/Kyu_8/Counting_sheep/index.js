eb5335b149520a488d6d846cb52d24da9f2729bb
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-17 21:49:49 +0200

    adds solution to Counting_sheep

---
+++ challenges/Kyu_8/Counting_sheep/index.js
@@ -0,0 +1,13 @@
+"use strict";
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.reduce((a, b) => b ? a + 1 : a, 0);
+}
+function countSheeps2(arr) {
+    return arr.filter(Boolean).length;
+}
+console.log(countSheeps([true, true, true, false,
+    true, true, true, true,
+    true, false, true, false,
+    true, false, false, true,
+    true, true, true, true,
+    false, false, true, true])); // 17
---
+++ challenges/Kyu_8/Counting_sheep/index.ts
@@ -0,0 +1,14 @@
+function countSheeps(arrayOfSheep: boolean[]): number {
+    return arrayOfSheep.reduce((a, b) => b ? a + 1 : a, 0);
+}
+
+function countSheeps2(arr: boolean[]): number {
+    return arr.filter(Boolean).length;
+}
+
+console.log(countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ])); // 17
