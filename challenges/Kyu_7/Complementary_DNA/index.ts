89ad61ed5063a591a8bdc3dc4770b70826e4b649
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-22 22:23:17 +0200

    adds solution for Complementary_DNA

---
+++ challenges/Kyu_7/Complementary_DNA/index.ts
@@ -0,0 +1,24 @@
+function DNAStrand(dna: string): string {
+	return dna.split('').map(char => {
+			switch(char) {
+				case 'A':
+					return 'T';
+				case 'T':
+					return 'A';
+				case 'C':
+					return 'G';
+				case 'G':
+					return 'C';
+			}
+		})
+		.join('');
+}
+
+/**
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+const DNAStrand = dna => dna.replace(/./g, c => pairs[c]); // "Dot" matches any character except line breaks; "g"-tag for global
+ */
+
+console.log(DNAStrand("AAAA")); // "TTTT","String AAAA is"
+console.log(DNAStrand("ATTGC")); // "TAACG","String ATTGC is"
+console.log(DNAStrand("GTAT")); // "CATA","String GTAT is"
