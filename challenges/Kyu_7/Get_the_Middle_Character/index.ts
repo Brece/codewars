a4e64658508acd2587135ced02c8ef78e8b8d475
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-24 00:31:27 +0200

    adds solution for Get_the_Middle_Character

---
+++ challenges/Kyu_7/Get_the_Middle_Character/index.ts
@@ -0,0 +1,9 @@
+function getMiddle(s: string): string {
+    const mid = s.length/2;
+    return s.length % 2 === 0 ? s.slice(mid - 1, mid + 1) : s[Math.round(mid - 1)];
+}
+
+console.log(getMiddle("test")); // "es"
+console.log(getMiddle("testing")); // "t"
+console.log(getMiddle("middle")); // "dd"
+console.log(getMiddle("A")); // "A"
