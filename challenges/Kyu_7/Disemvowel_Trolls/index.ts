e428e56c90975d53f0eeea9b17bc4fb6eac0c9df
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-24 00:12:15 +0200

    adds solution for Disemvowel_Trolls

---
+++ challenges/Kyu_7/Disemvowel_Trolls/index.ts
@@ -0,0 +1,6 @@
+function disemvowel(str: string): string {
+    return str.replace(/[aeiou]/gi, '');
+}
+console.log(disemvowel("This website is for losers LOL!")); // "Ths wbst s fr lsrs LL!"
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read")); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+console.log(disemvowel("What are you, a communist?")); // "Wht r y,  cmmnst?"
