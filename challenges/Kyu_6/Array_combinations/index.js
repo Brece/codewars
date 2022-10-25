7aca7770fc0caf3d4e8df3446b1e24f9cb49211b
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-25 06:00:38 +0200

    adds solution for Array_combinations

---
+++ challenges/Kyu_6/Array_combinations/index.js
@@ -0,0 +1,22 @@
+"use strict";
+// filter out dublicates of each sub-array by creating a Set and return its size in a new array
+// reduce the new array by multiplying the array items
+// this equates to the number ob possible combination since there are no dublicates
+function solve(arr) {
+    return (arr.map(subArr => {
+        let temp = new Set();
+        for (const i of subArr) {
+            temp.add(i);
+        }
+        ;
+        return temp.size;
+    })
+        .reduce((a, b) => a * b));
+}
+;
+console.log(solve([[1, 2], [4], [5, 6]])); // 4
+console.log(solve([[1, 2], [4, 4], [5, 6, 6]])); // 4
+console.log(solve([[1, 2], [3, 4], [5, 6]])); // 8
+console.log(solve([[1, 2, 3], [3, 4, 6, 6, 7], [8, 9, 10, 12, 5, 6]])); // 72
+//[ [1,2,3,4,5], [1,2,3], [1] ]; // 5*3*1 = 15
+//[ [8,9,10,12,5,6], [3,4,6,7] , [1,2,3] ]; // Length: 6*4*3 = 72
