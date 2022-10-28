f6c9433cc01f595e778a8e8d6bd45d3471e59a91
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-29 00:19:35 +0200

    adds solution for The_Supermarket_Queue

--- challenges/Kyu_6/The_Supermarket_Queue/index.js
+++ challenges/Kyu_6/The_Supermarket_Queue/index.js
@@ -1,39 +1,28 @@
 "use strict";
-// TODO:
-/*
-function queueTime(customers: number[], n: number): number {
-    if (n === 1) {
-        return customers.reduce((a, b) => a + b);
-    }
-
-    let sum = 0;
-    let currentCustomers = customers.splice(0, n).sort((a, b) => a - b);
-    let rest = customers.slice(n);
-    if (rest.length > 0) {
-        // substract smallest number from all customers
-        currentCustomers.map(n => n - currentCustomers[0] ? 0 : n - currentCustomers[0]);
-        // replace smallest number (all zeros) with next customer(s) and repeat until rest is empty
-        // store passed time in sum
-    }
-
-    return 0;
-}
-*/
 function queueTime(customers, n) {
+    if (customers.length === 0) {
+        return 0;
+    }
     // create temp array that holds as many items as there are queues
-    // the first n-customers are the initial values for each queue; e.g. [1,8,4,5] with n=2 results to temp=[1,8]
-    // find minimum in temp array and add the next customer to it; do this until the end of customer array reaches its end
+    let queues = [];
+    customers.forEach(person => {
+        // the first n-customers are the initial values for each queue; e.g. [1,8,4,5] with n=2 results to temp=[1,8]
+        if (queues.length < n) {
+            queues.push(person);
+            return;
+        }
+        // find minimum in temp array and add the next customer to it; do this until the end of customer array reaches its end
+        const min = Math.min(...queues);
+        queues[queues.indexOf(min)] += person;
+    });
     // find and return maximum of temp array
+    return Math.max(...queues);
 }
 const queueTime1 = queueTime([5, 3, 4], 1); // 12
 const queueTime2 = queueTime([10, 2, 3, 3], 2); // 10
 const queueTime3 = queueTime([2, 3, 10], 2); // 12
-// const queueTime4 = queueTime(); // 
-// const queueTime5 = queueTime(); // 
-// const queueTime6 = queueTime(); // 
+const queueTime4 = queueTime([], 6); // 
 console.log(queueTime1);
 console.log(queueTime2);
 console.log(queueTime3);
-// console.log(queueTime4);
-// console.log(queueTime5);
-// console.log(queueTime6);
+console.log(queueTime4);
