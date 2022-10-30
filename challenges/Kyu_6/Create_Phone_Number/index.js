2a5759e941eb0c96bd62df60c674a7f5183153b7
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-10-30 22:15:14 +0100

    adds solution for Create_Phone_Number

---
+++ challenges/Kyu_6/Create_Phone_Number/index.js
@@ -0,0 +1,31 @@
+"use strict";
+function createPhoneNumber(numbers) {
+    return numbers.map((num, index) => {
+        if (index === 0) {
+            return `(${num}`;
+        }
+        if (index === 2) {
+            return `${num}) `;
+        }
+        if (index === 5) {
+            return `${num}-`;
+        }
+        return num;
+    })
+        .join('');
+}
+/**
+function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+        format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+}
+ */
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
