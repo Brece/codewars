4145cc2c6c23f59299108d6c2fd985f92dd5ed56
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-02 23:24:08 +0100

    adds solution for Find_the_odd_int

---
+++ challenges/Kyu_6/Find_the_odd_int/index.ts
@@ -0,0 +1,15 @@
+function findOdd(arr: number[]): number {
+    let oddNum = arr[0];
+    let map = new Map();
+    arr.forEach(num => map.has(num) ? map.set(num, map.get(num) + 1) : map.set(num, 1));
+    for (const [key, value] of map) {
+        if(value % 2 === 1) {
+            return key;
+        }
+    }
+
+    return oddNum;
+}
+
+console.log(findOdd([7]), 7);
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]), 4);
