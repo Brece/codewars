3d127ac1efdd6bb5f25b5633592ccd5e0922ce60
Nui Ruppert <nuiruppert@MBP-von-Nui.fritz.box>
2022-11-03 23:54:14 +0100

    adds solution for Counting_Duplicates

---
+++ challenges/Kyu_6/Counting_Duplicates/index.js
@@ -0,0 +1,21 @@
+"use strict";
+function duplicateCount(text) {
+    let map = new Map();
+    let result = 0;
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i].toLowerCase();
+        map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
+    }
+    for (const [key, value] of map) {
+        if (value > 1) {
+            result++;
+        }
+    }
+    return result;
+}
+console.log(duplicateCount(""), 0);
+console.log(duplicateCount("abcde"), 0);
+console.log(duplicateCount("aabbcde"), 2);
+console.log(duplicateCount("aabBcde"), 2, "should ignore case");
+console.log(duplicateCount("Indivisibility"), 1);
+console.log(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent");
