298d5f6349f336f9275c0748e68d5b6e214b9ab9
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-04 22:27:54 +0100

    adds solution for Take_a_Ten_Minutes_Walk

---
+++ challenges/Kyu_6/Take_a_Ten_Minutes_Walk/index.ts
@@ -0,0 +1,17 @@
+function isValidWalk(walk: string[]): boolean {
+    if(walk.length !== 10) { return false }
+    let v = 0, h = 0;
+
+    walk.forEach(str => {
+        if(str === "n") { v++ } 
+        if(str === "s") { v-- }
+        if(str === "w") { h-- }
+        if(str === "e") { h++ }
+    });
+    return v === 0 && h === 0;
+}
+
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
+console.log(isValidWalk(['w']), 'should return false');
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
