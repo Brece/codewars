b380ceaa9d39c8cfd5c744bd1e045bc8a233103a
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-07 01:04:19 +0100

    adds setup for Replace_With_Alphabet_Position

--- challenges/Kyu_6/Replace_With_Alphabet_Position/index.js
+++ challenges/Kyu_6/Replace_With_Alphabet_Position/index.js
@@ -1,50 +1,13 @@
 "use strict";
-function upArray(arr) {
-    if (arr.find(num => num < 0 || num > 9) || arr.length === 0) {
-        return null;
-    }
-    let uppedArr = String(BigInt(arr.join('')) + BigInt(1))
-        .split('')
-        .map(num => Number(num));
-    if (uppedArr.length < arr.length) {
-        for (let i = 0; i <= arr.length - uppedArr.length; i++) {
-            uppedArr.unshift(0);
-        }
-    }
-    return uppedArr;
+// ASCII char code: a = 97, z = 122
+function alphabetPosition(text) {
+    return text.toLowerCase().split('')
+        .filter(char => {
+        let code = char.charCodeAt(0);
+        return !(code < 97) && !(code > 122);
+    })
+        .map(char => (char.charCodeAt(0) - 96).toString())
+        .join(' ');
 }
-/** solution without type conversions
-function upArray(arr) {
-    if ((typeof arr === 'undefined') || (arr === null) || (arr.length === 0)) {
-        return null;
-    }
-
-    for (var i = 0; i < arr.length; i++) {
-        if ((arr[i] < 0) || (typeof arr[i] !== 'number') || (arr[i] > 9)) {
-            return null;
-        }
-    }
-    // if its 9 and more than one digit we have to check all previous digits
-    // whether they are also a 9
-    for (var j = arr.length - 1; j > -1; j--) {
-        if (arr[j] !== 9) {
-            arr[j] = arr[j] + 1;
-            break;
-        } else {
-            arr[j] = 0;
-        }
-
-        if (j === 0) {
-            arr.unshift(1);
-        }
-    }
-    return arr;
-}
- */
-console.log(upArray([4, 3, 2, 5])); // [4,3,2,6]
-console.log(upArray([2, 3, 9, 9])); // [2,4,0,0]
-console.log(upArray([9, 9])); // [1,0,0]
-console.log(upArray([0, 7])); // [0,8]
-console.log(upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,1]
-console.log(upArray([1, -9])); // null
-console.log(upArray([1, 10])); // null
+console.log(alphabetPosition("The sunset sets at twelve o' clock."), "Expected: 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+console.log(alphabetPosition("The narwhal bacons at midnight."), "Expected: 20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
--- challenges/Kyu_6/Replace_With_Alphabet_Position/index.ts
+++ challenges/Kyu_6/Replace_With_Alphabet_Position/index.ts
@@ -1,6 +1,13 @@
-function alphabetPosition(text) {
-    return text;
+// ASCII char code: a = 97, z = 122
+function alphabetPosition(text: string): string {
+    return text.toLowerCase().split('')
+        .filter(char => {
+            let code = char.charCodeAt(0);
+            return !(code < 97) && !(code > 122);
+        })
+        .map(char => (char.charCodeAt(0) - 96).toString())
+        .join(' ');
 }
 
-console.log(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
-console.log(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+console.log(alphabetPosition("The sunset sets at twelve o' clock."), "Expected: 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+console.log(alphabetPosition("The narwhal bacons at midnight."), "Expected: 20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
