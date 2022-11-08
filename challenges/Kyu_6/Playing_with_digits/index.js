72331bca14ccb1cf6529f8f6ac6efac9447298f7
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-08 19:54:09 +0100

    adds solution for Playing_with_digits

---
+++ challenges/Kyu_6/Playing_with_digits/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function digPow(n, p) {
+    let arr = n.toString().split('');
+    let sum = 0;
+    arr.forEach((num, index) => {
+        sum += Number(num) ** (p + index);
+    });
+    return sum % n === 0 ? sum / n : -1;
+}
+console.log(digPow(89, 1), 1);
+console.log(digPow(92, 1), -1);
+console.log(digPow(46288, 3), 51);
