bd49d4421d598f220e6d2c623e603fb4c05895bd
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-12 22:31:01 +0100

    adds solution for Convert_string_to_camel_case

---
+++ challenges/Kyu_6/Convert_string_to_camel_case/index.ts
@@ -0,0 +1,13 @@
+function toCamelCase(str: string): string {
+    if (str.length === 0) { return '' }
+
+    return str
+        .split(/-|_/g)
+        .map((word, index) => index > 0 ? word[0].toUpperCase() + word.slice(1) : word)
+        .join('');
+}
+
+console.log(toCamelCase(''), '', "An empty string was provided but not returned");
+console.log(toCamelCase("the_stealth_warrior"), "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value");
+console.log(toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value");
+console.log(toCamelCase("A-B-C"), "ABC", "toCamelCase('A-B-C') did not return correct value");
