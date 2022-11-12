0725cabad1e303b4783308c3c128dfe6b2ce5379
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-12 23:19:31 +0100

    adds solution for Unique_In_Order

---
+++ challenges/Kyu_6/Unique_In_Order/index.ts
@@ -0,0 +1,22 @@
+/*
+function uniqueInOrder(iterable: string|number[]): (string|number)[] {
+    let arr = [...iterable];
+    let uniqueArr: (string|number)[] = [];
+
+    arr.forEach((item, index, array) => {
+        if(item !== array[index + 1]) {
+            uniqueArr.push(item);
+        }
+    });
+
+    return uniqueArr;
+}
+*/
+
+function uniqueInOrder(iterable: string|number[]): (string|number)[] {
+    return [...iterable].filter((item, index) => item !== iterable[index - 1]);
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B']);
+console.log(uniqueInOrder('ABBCcAD'), ['A', 'B', 'C', 'c', 'A', 'D']);
+console.log(uniqueInOrder([1,2,2,3,3]), [1,2,3]);
