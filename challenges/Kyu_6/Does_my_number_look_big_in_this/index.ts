d2be1d04d4d7dfdc81be37c82d34f3a9c2745a56
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-13 19:55:42 +0100

    adds solution for Does_my_number_look_big_in_this

---
+++ challenges/Kyu_6/Does_my_number_look_big_in_this/index.ts
@@ -0,0 +1,14 @@
+function narcissistic(value: number): boolean {
+    let arr = value.toString().split('');
+    let pow = arr.length;
+    let result = 0;
+
+    arr.forEach(n => result += Number(n)**pow);
+
+    return value === result;
+}
+
+console.log(narcissistic(7), true, "7 is narcissistic");
+console.log(narcissistic(371), true, "371 is narcissistic");
+console.log(narcissistic(153), true, "153 is narcissistic");
+console.log(narcissistic(1652), false, "1652 is not narcissistic");
