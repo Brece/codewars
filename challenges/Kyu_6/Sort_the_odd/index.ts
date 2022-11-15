e44df518185ad15d88d9800666d0a46db89c74f3
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-15 23:53:47 +0100

    adds solution for Sort_the_odd

---
+++ challenges/Kyu_6/Sort_the_odd/index.ts
@@ -0,0 +1,10 @@
+function sortArray(array: number[]): (number | undefined)[] {
+    let oddSorted = array.filter(n => n % 2).sort((a, b) => b - a);
+
+    return array.map(n => n % 2 ? oddSorted.pop() : n);
+}
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+console.log(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+console.log(sortArray([]),[]);
+console.log(sortArray([ -4, -18, -7, -37, -18, 24, 0, -32, 14, -20, 14 ]),[ -4, -18, -37, -7, -18, 24, 0, -32, 14, -20, 14 ]);
