e0b4c8cacec720fccb0ca7f4488e126173c387eb
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-18 00:02:53 +0100

    adds solution for Find_the_missing_letter

---
+++ challenges/Kyu_6/Find_the_missing_letter/index.ts
@@ -0,0 +1,11 @@
+function findMissingLetter(arr: string[]): string {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].charCodeAt(0) + 1 !== arr[i + 1].charCodeAt(0)) {
+            return String.fromCharCode(arr[i].charCodeAt(0) + 1);
+        }
+    }
+    return '';
+}
+
+console.log(findMissingLetter(['a','b','c','d','f']), 'e');
+console.log(findMissingLetter(['O','Q','R','S']), 'P');
