5adafc9883b51a027f0e978f2b58ff9ac89f9d75
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-21 20:39:19 +0100

    adds solution for Consecutive_strings

---
+++ challenges/Kyu_6/Consecutive_strings/index.js
@@ -0,0 +1,31 @@
+"use strict";
+function longestConsec(arr, k) {
+    let arrLength = arr.length;
+    let longestStr = '';
+    if (arrLength === 0 || k > arrLength || k <= 0) {
+        return longestStr;
+    }
+    // for(let i = 0; i <= arrLength - k; i++) {
+    //     let currentStr = '';
+    //     for(let j = 0; j < k; j++) {
+    //         currentStr += arr[i + j];
+    //     }
+    //     if(currentStr.length > longestStr.length) { longestStr = currentStr }
+    // }
+    for (let i = 0; i <= arrLength - k; i++) {
+        let currentStr = arr.slice(i, i + k).join('');
+        if (currentStr.length > longestStr.length) {
+            longestStr = currentStr;
+        }
+    }
+    return longestStr;
+}
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta");
+console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh");
+console.log(longestConsec([], 3), "");
+console.log(longestConsec(["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+console.log(longestConsec(["wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "");
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz");
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15), "");
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0), "");
