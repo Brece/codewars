66df97697d95604e4536d721f30a53f55fec4cdf
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-23 23:35:17 +0100

    adds solution for Title_Case

---
+++ challenges/Kyu_6/Title_Case/index.js
@@ -0,0 +1,20 @@
+"use strict";
+function titleCase(title, minorWords) {
+    if (title === '') {
+        return '';
+    }
+
+    let minorWordsSet = minorWords ? new Set(minorWords.toLowerCase().split(' ')) : new Set('');
+
+    return title.split(' ')
+        .map((w, i) => {
+            let newWord = w.toLowerCase();
+
+            return !minorWordsSet.has(newWord) || i === 0 ? newWord[0].toUpperCase() + newWord.slice(1) : newWord;
+        })
+        .join(' ');
+}
+console.log(titleCase(''), '');
+console.log(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings');
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows');
+console.log(titleCase('the quick brown fox'), 'The Quick Brown Fox');
