28a4bfaf149dea0c60726cbba3412411ebcf72b9
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-01 21:22:58 +0100

    adds solution for Rectangle_into_Squares

---
+++ challenges/Kyu_6/Rectangle_into_Squares/index.ts
@@ -0,0 +1,18 @@
+function sqInRect(lng: number, wdth: number): number[]|null {
+    if(lng === wdth) { return null }
+
+    let a = lng;
+    let b = wdth;
+    let result: number[] = [];
+
+    while (a > 0 && b > 0) {
+        result.push(Math.min(a,b));
+        a > b ? a -= b : b -= a;
+    }
+    return result;
+}
+
+console.log(sqInRect(5, 5), null);
+console.log(sqInRect(5, 3), [3, 2, 1, 1]);
+console.log(sqInRect(3, 5), [3, 2, 1, 1]);
+console.log(sqInRect(20, 14), [14, 6, 6, 2, 2, 2]);
