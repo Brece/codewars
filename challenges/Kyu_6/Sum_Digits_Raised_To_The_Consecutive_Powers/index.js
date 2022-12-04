2e43322984e0568ca630899835886ce62a1e7bd6
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-04 17:45:02 +0100

    adds solution for Sum_Digits_Raised_To_The_Consecutive_Powers

---
+++ challenges/Kyu_6/Sum_Digits_Raised_To_The_Consecutive_Powers/index.js
@@ -0,0 +1,17 @@
+"use strict";
+function sumDigPow(a, b) {
+    let result = [];
+    for (let i = a; i <= b; i++) {
+        if (i.toString().split('').reduce((x, y, index) => x + Number(y) ** (index + 1), 0) === i) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
+console.log(sumDigPow(10, 100), [89]);
+console.log(sumDigPow(90, 100), []);
+console.log(sumDigPow(90, 150), [135]);
+console.log(sumDigPow(50, 150), [89, 135]);
+console.log(sumDigPow(10, 150), [89, 135]);
