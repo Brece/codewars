9ff33478afb751d02efdf8453ea561969bbb3ae4
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-05 21:15:10 +0100

    adds solution for Data_Reverse

---
+++ challenges/Kyu_6/Data_Reverse/index.ts
@@ -0,0 +1,11 @@
+function dataReverse(data: number[]): number[] {
+    let result: number[] = [];
+
+    for(let i = data.length; 0 <= i ; i -= 8) {
+        result = result.concat(data.slice(i - 8, i));
+    }
+    return result;
+}
+
+console.log(dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]), [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]);
+console.log(dataReverse([0,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1]), [0,0,1,0,1,0,0,1,0,0,1,1,0,1,1,0]);
