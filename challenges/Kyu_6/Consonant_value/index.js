528f59606d33997d9c13a651c52902109e3f8999
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-09 21:55:07 +0100

    adds solution for Consonant_value

---
+++ challenges/Kyu_6/Consonant_value/index.js
@@ -0,0 +1,18 @@
+"use strict";
+function solve(str) {
+    let subStr = str.split(/[aeiou]+/)
+        .map(sub => {
+        if (sub.length) {
+            return sub.split('').reduce((a, b) => a + b.charCodeAt(0) - 96, 0);
+        }
+        return 0;
+    });
+    return Math.max(...subStr);
+}
+console.log(solve("zodiac"), 26);
+console.log(solve("chruschtschov"), 80);
+console.log(solve("khrushchev"), 38);
+console.log(solve("strength"), 57);
+console.log(solve("catchphrase"), 73);
+console.log(solve("twelfthstreet"), 103);
+console.log(solve("mischtschenkoana"), 80);
