d05b360cea032a887b579742f1ad1f8105d4d3ca
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-20 21:44:09 +0100

    adds solution for Find_the_next_perfect_square

---
+++ challenges/Kyu_7/Find_the_next_perfect_square/index.ts
@@ -0,0 +1,11 @@
+function findNextSquare(sq: number): number {
+    const squareRoot = Math.sqrt(sq);
+    return squareRoot === Math.floor(squareRoot) ? Math.pow(squareRoot + 1, 2) : -1;
+}
+
+console.log(findNextSquare(121), 144);
+console.log(findNextSquare(625), 676);
+console.log(findNextSquare(319225), 320356);
+console.log(findNextSquare(15241383936), 15241630849);
+console.log(findNextSquare(155), -1);
+console.log(findNextSquare(342786627), -1);
