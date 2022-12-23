faed2fc16bd31c75816fb5df73b6a675226b8bba
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-23 22:38:05 +0100

    adds solution for Odd_or_Even

---
+++ challenges/Kyu_7/Odd_or_Even/index.js
@@ -0,0 +1,19 @@
+"use strict";
+function oddOrEven(arr) {
+    return arr.reduce((a, b) => a + b, 0) % 2 ? 'odd' : 'even';
+}
+console.log(oddOrEven([0]), 'even');
+console.log(oddOrEven([1]), 'odd');
+console.log(oddOrEven([]), 'even');
+console.log(oddOrEven([0, 1, 5]), 'even');
+console.log(oddOrEven([0, 1, 3]), 'even');
+console.log(oddOrEven([1023, 1, 2]), 'even');
+console.log(oddOrEven([0, -1, -5]), 'even');
+console.log(oddOrEven([0, -1, -3]), 'even');
+console.log(oddOrEven([-1023, 1, -2]), 'even');
+console.log(oddOrEven([0, 1, 2]), 'odd');
+console.log(oddOrEven([0, 1, 4]), 'odd');
+console.log(oddOrEven([1023, 1, 3]), 'odd');
+console.log(oddOrEven([0, -1, 2]), 'odd');
+console.log(oddOrEven([0, 1, -4]), 'odd');
+console.log(oddOrEven([-1023, -1, 3]), 'odd');
