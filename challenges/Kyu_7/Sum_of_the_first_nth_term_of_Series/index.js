8cafb58173ada693b24b1bf93675348350aadf2e
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-24 15:56:16 +0100

    adds solution for Sum_of_the_first_nth_term_of_Series

---
+++ challenges/Kyu_7/Sum_of_the_first_nth_term_of_Series/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// series is: 1/((n - 1) * 2 + n)
+// equals to: 1/(3n - 2)
+function SeriesSum(n) {
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += 1 / (3 * i - 2);
+    }
+    return result.toFixed(2);
+}
+console.log(SeriesSum(1), "1.00");
+console.log(SeriesSum(2), "1.25");
+console.log(SeriesSum(3), "1.39");
+console.log(SeriesSum(4), "1.49");
