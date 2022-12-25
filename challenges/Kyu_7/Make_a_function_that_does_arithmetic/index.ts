048ff5d12a8eca62706135dff547407e0aeab890
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-25 17:01:10 +0100

    adds solution for Make_a_function_that_does_arithmetic

---
+++ challenges/Kyu_7/Make_a_function_that_does_arithmetic/index.ts
@@ -0,0 +1,15 @@
+function arithmetic(a: number, b: number, operator: string): number {
+    const operators: { [key: string]: number} = {
+        'add': a + b,
+        'subtract': a - b,
+        'multiply': a * b,
+        'divide': a / b
+    };
+
+    return operators[operator];
+}
+
+console.log(arithmetic(1, 2, "add"), 3, "'add' should return the two numbers added together!");
+console.log(arithmetic(8, 2, "subtract"), 6, "'subtract' should return a minus b!");
+console.log(arithmetic(5, 2, "multiply"), 10, "'multiply' should return a multiplied by b!");
+console.log(arithmetic(8, 2, "divide"), 4, "'divide' should return a divided by b!");
