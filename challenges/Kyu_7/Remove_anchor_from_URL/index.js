69838b1bb57c71c622f71b0e2fbe268060d7e8fa
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-26 17:03:15 +0100

    adds solution for Remove_anchor_from_URL

---
+++ challenges/Kyu_7/Remove_anchor_from_URL/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/** split method
+function removeUrlAnchor(url: string): string {
+    return url.split('#')[0];
+}
+*/
+// using regular expressions to replace everything that matches the regex with an empty string
+function removeUrlAnchor(url) {
+    return url.replace(/#.*/gi, "");
+}
+console.log(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com');
+console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'), 'www.codewars.com/katas/?page=1');
+console.log(removeUrlAnchor('www.codewars.com/katas/'), 'www.codewars.com/katas/');
