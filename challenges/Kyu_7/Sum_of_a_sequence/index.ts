4a417596cc92c02f90ad8b07a0088e5f22755787
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-26 17:13:13 +0100

    adds solution for Sum_of_a_sequence

---
+++ challenges/Kyu_7/Sum_of_a_sequence/index.ts
@@ -0,0 +1,11 @@
+function sequenceSum(begin: number, end: number, step: number): number  {
+    let sum = 0;
+    for(let i = begin; i <= end; i += step) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sequenceSum(2, 6, 2), 12);
+console.log(sequenceSum(1, 5, 1), 15);
+console.log(sequenceSum(1, 5, 3), 5);
---
+++ challenges/Kyu_7/Sum_of_a_sequence/sumOfASequence.test.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(sequenceSum(2, 6, 2), 12)
+    assert.strictEqual(sequenceSum(1, 5, 1), 15)
+    assert.strictEqual(sequenceSum(1, 5, 3), 5)  
+  })
+});
