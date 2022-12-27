3d1232d22528db523ae5b15fbc0a1dc34c424582
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-27 22:14:11 +0100

    adds solution for Small_enough

--- challenges/Kyu_7/Two_Oldest_Ages/index.js
+++ challenges/Kyu_7/Two_Oldest_Ages/index.js
@@ -1,6 +1,7 @@
 "use strict";
 function twoOldestAges(ages) {
-    return ages.sort((a, b) => a - b).slice(-2);
+    let sorted = ages.sort((a, b) => a - b);
+    return [sorted[sorted.length - 2], sorted[sorted.length - 1]];
 }
 console.log(twoOldestAges([1, 5, 87, 45, 8, 8]), [45, 87]);
 console.log(twoOldestAges([6, 5, 83, 5, 3, 18]), [18, 83]);
