0a67627a1ecc5f921203c344da345e17ca160d4a
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-28 16:35:51 +0100

    adds solution for Maximum_Multiple

---
+++ challenges/Kyu_7/Maximum_Multiple/index.ts
@@ -0,0 +1,10 @@
+function maxMultiple(divisor: number, bound: number): number {
+    return Math.floor(bound/divisor) * divisor;
+}
+
+console.log(maxMultiple(2,7),6);
+console.log(maxMultiple(3,10),9);
+console.log(maxMultiple(7,17),14);
+console.log(maxMultiple(10,50),50);
+console.log(maxMultiple(37,200),185);
+console.log(maxMultiple(7,100),98);
