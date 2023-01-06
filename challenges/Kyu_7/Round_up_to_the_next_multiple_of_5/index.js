ef00eae84e60da9a1d6093787bff5d9a05c4efb9
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2023-01-07 00:19:26 +0100

    adds solution for Round_up_to_the_next_multiple_of_5

---
+++ challenges/Kyu_7/Round_up_to_the_next_multiple_of_5/index.js
@@ -0,0 +1,14 @@
+"use strict";
+function roundToNext5(n) {
+    if (n < 0) {
+        return n % 5 ? n - (n % 5) : n;
+    }
+    return n % 5 ? n + 5 - (n % 5) : n;
+}
+/**
+function roundToNext5(n){
+    return Math.ceil(n/5)*5;
+}
+ */
+const tests = [[0, 0], [1, 5], [-1, 0], [-7, -5], [-5, -5], [3, 5], [5, 5], [7, 10], [20, 20], [39, 40], [990, 990], [121, 125], [555, 555]];
+tests.forEach(([n, result]) => console.log(roundToNext5(n), result));
