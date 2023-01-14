78f6bf5cf42e30b5236d9e3ee020abb7e40409f8
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2023-01-14 23:58:36 +0100

    adds solution for Two_fighters_one_winner

---
+++ challenges/Kyu_7/Two_fighters_one_winner/index.js
@@ -0,0 +1,53 @@
+"use strict";
+class Fighter {
+    name;
+    health;
+    damagePerAttack;
+    constructor(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function () { return this.name; };
+    }
+}
+;
+/**
+function declareWinner(fighter1: Fighter, fighter2: Fighter, firstAttacker: string): string {
+    let fighter1Health = fighter1.health;
+    let fighter2Health = fighter2.health;
+
+    if(firstAttacker === fighter1.name) {
+        while(fighter1Health > 0 && fighter2Health > 0) {
+            fighter2Health -= fighter1.damagePerAttack;
+            if(fighter2Health <= 0) { return fighter1.name }
+            fighter1Health -= fighter2.damagePerAttack;
+            if(fighter1Health <= 0) { return fighter2.name }
+        }
+    } else {
+        while(fighter1Health > 0 && fighter2Health > 0) {
+            fighter1Health -= fighter2.damagePerAttack;
+            if(fighter1Health <= 0) { return fighter2.name }
+            fighter2Health -= fighter1.damagePerAttack;
+            if(fighter2Health <= 0) { return fighter1.name }
+        }
+    }
+    return '';
+}
+*/
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    let fraction1 = Math.ceil(fighter1.health / fighter2.damagePerAttack);
+    let fraction2 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
+    if (fraction1 > fraction2) {
+        return fighter1.name;
+    }
+    if (fraction1 < fraction2) {
+        return fighter2.name;
+    }
+    return firstAttacker;
+}
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"), "Lew");
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"), "Harry");
+console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"), "Harald");
+console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"), "Harald");
+console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"), "Harald");
+console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"), "Harald");
