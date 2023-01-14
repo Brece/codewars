76269fc94a65e10ed2ea35a7f2dd589e055f2289
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2023-01-14 22:03:57 +0100

    adds solution for Maximum_Length_Difference

---
+++ challenges/Kyu_7/Maximum_Length_Difference/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function mxdiflg(a1, a2) {
+    if (!a1.length || !a2.length) {
+        return -1;
+    }
+    const lengthArr1 = a1.map(e => e.length);
+    const lengthArr2 = a2.map(e => e.length);
+    return Math.max(Math.abs(Math.max(...lengthArr1) - Math.min(...lengthArr2)), Math.abs(Math.max(...lengthArr2) - Math.min(...lengthArr1)));
+}
+const s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
+const s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
+console.log(mxdiflg(s1, s2), 13);
