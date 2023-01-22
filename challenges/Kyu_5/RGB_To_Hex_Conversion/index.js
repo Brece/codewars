d551a2d689ccce077c7a5d1225f1d921600e7ad0
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2023-01-22 23:30:37 +0100

    adds solution for RGB_To_Hex_Conversion

---
+++ challenges/Kyu_5/RGB_To_Hex_Conversion/index.js
@@ -0,0 +1,18 @@
+"use strict";
+function rgb(r, g, b) {
+    return [...arguments].map(n => {
+        let num = n;
+        if (num < 0) {
+            num = 0;
+        }
+        if (num > 255) {
+            num = 255;
+        }
+        let hex = num.toString(16).toUpperCase();
+        return hex.length === 1 ? '0' + hex : hex;
+    }).join('');
+}
+console.log(rgb(0, 0, 0), '000000');
+console.log(rgb(0, 0, -20), '000000');
+console.log(rgb(300, 255, 255), 'FFFFFF');
+console.log(rgb(173, 255, 47), 'ADFF2F');
